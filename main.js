1dd731cf11b81d1094934bda532ede09fd94147f
kudzaimhlanga <37145094+kudzaimhlanga@users.noreply.github.com>
2023-02-14 14:28:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Experience from "./Experience/Experience.js";
 
+//initialize the experience
 const canvas = document.querySelector(".experience");
 const experience = new Experience(canvas);
